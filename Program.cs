2bb1e8e96a5e3bc4156676ff633205560ec62d2b
Rickie Le <rickienthefishies@gmail.com>
2021-03-02 14:22:51 -0600

    the moose talksga

--- Program.cs
+++ Program.cs
@@ -11,6 +11,7 @@ void Main()
 
     // Magic Moose asks you a question
     MagicMooseAsks();
+
 }
 
 
@@ -26,17 +27,19 @@ void MagicMooseAsks()
     // If statement - If space is empty then do this, else run MagicMooseAnswer
     if (string.IsNullOrWhiteSpace(YourQuestion))
     {
-        MagicalMooseWisdom("I wasn't going to answer anyway.");
+        MagicalMooseWisdom("I wasn't going to respond anyway.");
     }
     else
     {
-        MagicalMooseWisdom("What the");
+        MagicalMooseWisdom(MagicMooseAnswer());
     }
 }
 
 // Function for the Magic Moose to answer
 string MagicMooseAnswer()
 {
+    string Wisdom = "";
+
     // Generate a random number
     Random Number = new Random();
     // Add minimum and max values
@@ -46,73 +49,73 @@ string MagicMooseAnswer()
     switch (RandomNumber)
     {
         case 0:
-            Console.WriteLine("As I see it, yes.");
+            Wisdom = "As I see it, yes.";
             break;
         case 1:
-            Console.WriteLine("Ask again later.");
+            Wisdom = "Ask again later.";
             break;
         case 2:
-            Console.WriteLine("Better not tell you now.");
+            Wisdom = "Better not tell you now.";
             break;
         case 3:
-            Console.WriteLine("Cannot predict now.");
+            Wisdom = "Cannot predict now.";
             break;
         case 4:
-            Console.WriteLine("Concentrate and ask again.");
+            Wisdom = "Concentrate and ask again.";
             break;
         case 5:
-            Console.WriteLine("Don’t count on it.");
+            Wisdom = "Don’t count on it.";
             break;
         case 6:
-            Console.WriteLine("It is certain.");
+            Wisdom = "It is certain.";
             break;
         case 7:
-            Console.WriteLine("It is decidedly so.");
+            Wisdom = "It is decidedly so.";
             break;
         case 8:
-            Console.WriteLine("Most likely.");
+            Wisdom = "Most likely.";
             break;
         case 9:
-            Console.WriteLine("My reply is no.");
+            Wisdom = "My reply is no.";
             break;
         case 10:
-            Console.WriteLine("My sources say no.");
+            Wisdom = "My sources say no.";
             break;
         case 11:
-            Console.WriteLine("Outlook not so good.");
+            Wisdom = "Outlook not so good.";
             break;
         case 12:
-            Console.WriteLine("Outlook good.");
+            Wisdom = "Outlook good.";
             break;
         case 13:
-            Console.WriteLine("Reply hazy, try again.");
+            Wisdom = "Reply hazy, try again.";
             break;
         case 14:
-            Console.WriteLine("Signs point to yes.");
+            Wisdom = "Signs point to yes.";
             break;
         case 15:
-            Console.WriteLine("Very doubtful.");
+            Wisdom = "Very doubtful.";
             break;
         case 16:
-            Console.WriteLine("Without a doubt.");
+            Wisdom = "Without a doubt.";
             break;
         case 17:
-            Console.WriteLine("Yes.");
+            Wisdom = "Yes.";
             break;
         case 18:
-            Console.WriteLine("Yes – definitely..");
+            Wisdom = "Yes – definitely..";
             break;
         case 19:
-            Console.WriteLine("You may rely on it.");
+            Wisdom = "You may rely on it.";
             break;
-    }
+    };
 
-    return RandomNumber;
+    return Wisdom;
 }
 
 
 // Function for Magic Moose to provide an answer
-void MagicalMooseWisdom(string BitOfWisdom)
+void MagicalMooseWisdom(string Wisdom)
 {
     Console.WriteLine($@"
                                                 _.--^^^--,
@@ -131,7 +134,7 @@ void MagicalMooseWisdom(string BitOfWisdom)
                         '-'    | /       \ |
                                 |  .-. .-.  |
                                 \ / o| |o \ /
-                                |   / \   |    {BitOfWisdom}
+                                |   / \   |    {Wisdom}
                                 / `^`   `^` \
                                 /             \
                                 | '._.'         \
